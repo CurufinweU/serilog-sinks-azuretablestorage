f9dbac01b38e3955b7c20cafdcba6ac23e306727
Sergey Yerdekov <yerdekov@gmail.com>
2018-09-08 20:23:26 +0000

    DefaultBatchPostingLimit 50 to 60

--- src/Serilog.Sinks.AzureTableStorage/LoggerConfigurationAzureTableStorageExtensions.cs
+++ src/Serilog.Sinks.AzureTableStorage/LoggerConfigurationAzureTableStorageExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2014 Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ namespace Serilog
         /// A reasonable default for the number of events posted in
         /// each batch.
         /// </summary>
-        public const int DefaultBatchPostingLimit = 50;
+        public const int DefaultBatchPostingLimit = 60;
 
         /// <summary>
         /// A reasonable default time to wait between checking for event batches.
@@ -67,7 +67,7 @@ namespace Serilog
             var sink = writeInBatches ?
                 (ILogEventSink)new AzureBatchingTableStorageSink(storageAccount, formatProvider,  batchPostingLimit ?? DefaultBatchPostingLimit, period ?? DefaultPeriod, storageTableName) :
                 new AzureTableStorageSink(storageAccount, formatProvider, storageTableName);
-            
+
             return loggerConfiguration.Sink(sink, restrictedToMinimumLevel);
         }
 
